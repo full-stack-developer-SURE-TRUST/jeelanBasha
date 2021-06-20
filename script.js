2fc941434168b6b4ad287c6fcd96719e065ab7a3
jeelanSYED23 <84025839+jeelanSYED23@users.noreply.github.com>
2021-06-20 13:56:37 +0700

    color flipper

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const colors = ["#F08080","#FA8072","#E9967A","#FFA07A"];
+const btn = document.getElementById("btn");
+const color= document.querySelector(".color");
+
+btn.addEventListener("click",function(){
+  const randonNumber= getradomNumber();
+
+  document.body.style.background = colors[randonNumber];
+  color.textContent = colors[randonNumber];
+});
+
+function getradomNumber() {
+  return Math.floor(Math.random()*colors.length);
+}
